33708567ef0c3ce82cc9d4e5827677e732deb1a2
Jozef Steinhübl (xHyroM) <xhyrom.hyro@gmail.com>
2023-11-03 18:29:38 +0100

    handle big incidents

--- index.ts
+++ index.ts
@@ -1,19 +1,26 @@
-import fetch from 'node-fetch';
+import fetch from "node-fetch";
 import * as fs from "fs";
-import {ColorResolvable, DiscordAPIError, MessageEmbed, WebhookClient} from "discord.js";
-import {time} from "@discordjs/builders";
-import {join} from "node:path";
+import {
+  ColorResolvable,
+  DiscordAPIError,
+  MessageEmbed,
+  WebhookClient,
+} from "discord.js";
+import { time } from "@discordjs/builders";
+import { join } from "node:path";
 import * as config from "./config.json";
 const _ = require("lodash");
 
 const apiUrl = "https://www.cloudflarestatus.com/api/v2/incidents.json";
 const cacheFileName = join(__dirname, "messages.json");
 
-const ignoreDays = config["ignoreDays"]??30;
+const ignoreDays = config["ignoreDays"] ?? 30;
 const ignoreTime = ignoreDays * 86400000;
-console.log(`Ignoring incidents from ${ignoreDays} days ago (${ignoreTime} ms).`);
+console.log(
+  `Ignoring incidents from ${ignoreDays} days ago (${ignoreTime} ms).`
+);
 
-const webhookClient = new WebhookClient({url: config.url});
+const webhookClient = new WebhookClient({ url: config.url });
 
 /**
  * Checks if a message exists for the given incident. If so, the message will be updated, if there are new updates to
@@ -21,52 +28,52 @@ const webhookClient = new WebhookClient({url: config.url});
  * @param incident - The incident to check
  */
 async function checkIncident(incident: any) {
-    let id = incident.id;
-    const incidentUpdate = Date.parse(incident.updated_at);
+  let id = incident.id;
+  const incidentUpdate = Date.parse(incident.updated_at);
 
-    // check if update is too old
-    if (Date.now() - incidentUpdate > ignoreTime) {
-        console.debug(`Skipping update of incident ${id} because it's too old.`);
-        return;
-    }
+  // check if update is too old
+  if (Date.now() - incidentUpdate > ignoreTime) {
+    console.debug(`Skipping update of incident ${id} because it's too old.`);
+    return;
+  }
 
-    // check if message exists, if not create a new one
-    let messageId = await getMessageIdOfIncident(id);
-    if (messageId === undefined) {
-        await createMessage(incident);
-        return;
-    }
+  // check if message exists, if not create a new one
+  let messageId = await getMessageIdOfIncident(id);
+  if (messageId === undefined) {
+    await createMessage(incident);
+    return;
+  }
 
-    // fetch the old message
-    let message = undefined;
-    try {
-        message = await webhookClient.fetchMessage(messageId);
-    } catch (e) {
-        // message most likely was deleted - send a new one
-        if (e === DiscordAPIError) {
-            await createMessage(incident);
-            return;
-        }
+  // fetch the old message
+  let message = undefined;
+  try {
+    message = await webhookClient.fetchMessage(messageId);
+  } catch (e) {
+    // message most likely was deleted - send a new one
+    if (e === DiscordAPIError) {
+      await createMessage(incident);
+      return;
     }
+  }
 
-    // message does not exist anymore - send a new one
-    if (message === undefined) {
-        await createMessage(incident);
-        return;
-    }
+  // message does not exist anymore - send a new one
+  if (message === undefined) {
+    await createMessage(incident);
+    return;
+  }
 
-    // check timestamps in footer of embed to determine if we should update
-    if (message.embeds.length > 0) {
-        const messageUpdate = Date.parse(message.embeds[0].timestamp);
-        const diff = messageUpdate - incidentUpdate;
-        if (diff !== 0) {
-            await updateMessage(message, incident);
-            return;
-        }
-    } else {
-        // message contains no embeds - update message
-        await updateMessage(message, incident);
+  // check timestamps in footer of embed to determine if we should update
+  if (message.embeds.length > 0) {
+    const messageUpdate = Date.parse(message.embeds[0].timestamp);
+    const diff = messageUpdate - incidentUpdate;
+    if (diff !== 0) {
+      await updateMessage(message, incident);
+      return;
     }
+  } else {
+    // message contains no embeds - update message
+    await updateMessage(message, incident);
+  }
 }
 
 /**
@@ -74,31 +81,52 @@ async function checkIncident(incident: any) {
  * @param incident
  * @return {MessageEmbed} - the newly constructed EmbedMessage
  */
-function buildIncidentEmbed(incident: any) : MessageEmbed {
-    const embed = new MessageEmbed()
-        .setTitle(incident.name)
-        .setURL(incident.shortlink)
-        .setColor(getStatusColor(incident.status))
-        .setFooter(incident.id)
-        .setTimestamp(incident.updated_at);
+function buildIncidentEmbed(incident: any): MessageEmbed {
+  const embed = new MessageEmbed()
+    .setTitle(incident.name)
+    .setURL(incident.shortlink)
+    .setColor(getStatusColor(incident.status))
+    .setFooter(incident.id)
+    .setTimestamp(incident.updated_at);
 
-    // collect affected components
-    let components = [];
-    for (let i in incident.components) {
-        let component = incident.components[i];
-        components.push(component.name);
+  // collect affected components
+  let components = [];
+  for (let i in incident.components) {
+    let component = incident.components[i];
+    components.push(component.name);
+  }
+
+  embed.setDescription(
+    `• Impact: ${incident.impact}\n• Affected Components: ${components.join(
+      ", "
+    )}`
+  );
+
+  // collect incident updates
+  for (let i in incident.incident_updates) {
+    let update = incident.incident_updates[i];
+    let timeString = " (" + time(new Date(update.created_at), "R") + ")";
+    embed.addField(_.startCase(update.status) + timeString, update.body, false);
+  }
+
+  if (embed.fields.length > 25) {
+    embed.fields.pop();
+  }
+
+  while (
+    embed.fields.map((f) => f.value.length).reduce((a, b) => a + b, 0) > 6000
+  ) {
+    embed.fields.pop();
+  }
+
+  for (const field of embed.fields) {
+    if (field.value.length > 1024) {
+      field.value = field.value.substring(0, 1021) + "...";
     }
+  }
 
-    embed.setDescription(`• Impact: ${incident.impact}\n• Affected Components: ${components.join(", ")}`);
-
-    // collect incident updates
-    for (let i in incident.incident_updates) {
-        let update = incident.incident_updates[i];
-        let timeString = " (" + time(new Date(update.created_at), "R") + ")";
-        embed.addField(_.startCase(update.status) + timeString, update.body, false);
-    }
-    embed.fields.reverse();
-    return embed;
+  embed.fields.reverse();
+  return embed;
 }
 
 /**
@@ -107,12 +135,14 @@ function buildIncidentEmbed(incident: any) : MessageEmbed {
  * @param incident - The incident the message should represent
  */
 async function createMessage(incident: any) {
-    const id = incident.id;
-    const json = await readMessagesFile();
-    const messageId = await sendIncident(incident);
-    json[id] = messageId;
-    console.log(`Created new message for incident ${id} with message-id ${messageId}.`);
-    fs.writeFileSync(cacheFileName, JSON.stringify(json, null, 4));
+  const id = incident.id;
+  const json = await readMessagesFile();
+  const messageId = await sendIncident(incident);
+  json[id] = messageId;
+  console.log(
+    `Created new message for incident ${id} with message-id ${messageId}.`
+  );
+  fs.writeFileSync(cacheFileName, JSON.stringify(json, null, 4));
 }
 
 /**
@@ -121,28 +151,28 @@ async function createMessage(incident: any) {
  * @param incident - The incident the message contains
  */
 async function updateMessage(message, incident: any) {
-    let id = incident.id;
-    console.log(`Updating message of incident ${id}.`);
-    await webhookClient.editMessage(message, {
-        embeds: [buildIncidentEmbed(incident)],
-    });
+  let id = incident.id;
+  console.log(`Updating message of incident ${id}.`);
+  await webhookClient.editMessage(message, {
+    embeds: [buildIncidentEmbed(incident)],
+  });
 }
 
 /**
  * Runs the checks for updated incidents.
  */
 async function start() {
-    let obj = await fetchIncidents();
+  let obj = await fetchIncidents();
 
-    let incidents = obj.incidents.reverse();
-    for (let i in incidents) {
-        let incident = incidents[i];
-        try {
-            await checkIncident(incident);
-        } catch (e) {
-            console.error("Could not check incident.", e);
-        }
+  let incidents = obj.incidents.reverse();
+  for (let i in incidents) {
+    let incident = incidents[i];
+    try {
+      await checkIncident(incident);
+    } catch (e) {
+      console.error("Could not check incident.", e);
     }
+  }
 }
 
 /**
@@ -151,27 +181,27 @@ async function start() {
  * @return {String} id - The id of the newly created message
  */
 async function sendIncident(incident: any) {
-    const res = await webhookClient.send({
-        content: `<@&1157293690407092324>`,
-        embeds: [buildIncidentEmbed(incident)],
-    });
-    return res.id;
+  const res = await webhookClient.send({
+    content: `<@&1157293690407092324>`,
+    embeds: [buildIncidentEmbed(incident)],
+  });
+  return res.id;
 }
 
 /**
  * Checks if the message cache file exists. If not it will create a new one.
  */
 async function checkFile() {
-    if (fs.existsSync(cacheFileName)) return;
-    fs.writeFileSync(cacheFileName, JSON.stringify({}));
+  if (fs.existsSync(cacheFileName)) return;
+  fs.writeFileSync(cacheFileName, JSON.stringify({}));
 }
 
 /**
  * Reads the message cache file and returns its contents as json.
  */
 async function readMessagesFile() {
-    const data = fs.readFileSync(cacheFileName, "utf-8");
-    return JSON.parse(data);
+  const data = fs.readFileSync(cacheFileName, "utf-8");
+  return JSON.parse(data);
 }
 
 /**
@@ -180,15 +210,15 @@ async function readMessagesFile() {
  * @return {String | undefined} - The message id or undefined if it was not cached
  */
 async function getMessageIdOfIncident(id: string) {
-    return (await readMessagesFile())[id];
+  return (await readMessagesFile())[id];
 }
 
 /**
  * Fetches the discord-status api and returns the result as json.
  */
 async function fetchIncidents() {
-    let res = await fetch(apiUrl);
-    return await res.json();
+  let res = await fetch(apiUrl);
+  return await res.json();
 }
 
 /**
@@ -196,14 +226,23 @@ async function fetchIncidents() {
  * @param {String} status - The status of an incident
  * @return {ColorResolvable} color - The color corresponding to the given status
  */
-function getStatusColor(status: string) : ColorResolvable {
-    switch (status) {
-        case "resolved": return "#06a51b";
-        case "monitoring": return "#a3a506";
-        case "identified": return "#a55806";
-    }
-    return "#a50626";
+function getStatusColor(status: string): ColorResolvable {
+  switch (status) {
+    case "resolved":
+      return "#06a51b";
+    case "monitoring":
+      return "#a3a506";
+    case "identified":
+      return "#a55806";
+  }
+  return "#a50626";
 }
 
 // check if the message cache file exists, then start the program
-checkFile().then(() => start().then(() => console.log("Done.")).catch(console.error)).catch(console.error);
+checkFile()
+  .then(() =>
+    start()
+      .then(() => console.log("Done."))
+      .catch(console.error)
+  )
+  .catch(console.error);
